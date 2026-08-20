db7254312b99e6267eced8a5bfc8cd77e7842873
Evgenia <128167465+FilSado@users.noreply.github.com>
2026-08-20 21:54:58 +0500

    second commit

---
+++ dzDOM/js/dom.js
@@ -0,0 +1,83 @@
+import { todoKeys } from "./constants.js";
+import { createTodo, completeTodoById, deleteTodoById } from "./service.js";
+import { setTodosToLocalStorage } from "./storage.js";
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todosElement = document.querySelector(".todos");
+
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+
+const createTodoElement = (todo) => {
+	const todoElement = document.createElement("li");
+	todoElement.classList.add("todo");
+	todoElement.dataset.id = todo[todoKeys.id];
+	todoElement.innerHTML = `
+  <div class="todo-text">${todo[todoKeys.text]}</div>
+  <div class="todo-actions">
+    <button class="button-complete button">&#10004;</button>
+    <button class="button-delete button">&#10006;</button>
+  `;
+
+	return todoElement;
+};
+
+export const renderTodos = (todos) => {
+	todosElement.innerHTML = "";
+	todos.forEach((todo) => {
+		const todoElement = createTodoElement(todo);
+		if (todo[todoKeys.is_completed]) {
+			todoElement.classList.add("completed");
+		}
+		todoElement.prepend(todoElement);
+	});
+};
+
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+
+const handleCreateTodo = (todos, text) => {
+	const todo = createTodo(todos, text);
+	const todoElement = createTodoElement(todo);
+	setTodosToLocalStorage(todos);
+	todosElement.prepend(todoElement);
+};
+
+export const initTodoHandlers = (todos) => {
+	// Отслеживаем отправку формы
+
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+
+		const text = input.value.trim();
+
+		if (!text) {
+			return;
+		}
+
+		handleCreateTodo(todos, text);
+
+		input.value = "";
+	});
+
+	// Делегирование событий
+	todosElement.addEventListener("click", ({ target }) => {
+		const todo = target.closest(".todo");
+		if (!todo) return;
+
+		const todoId = Number(todo.dataset.id);
+
+		if (target.matches(".button-complete")) {
+			completeTodoById(todos, todoId);
+			setTodosToLocalStorage(todos);
+			todo.classList.toggle("completed");
+		}
+
+		if (target.matches(".button-delete")) {
+			deleteTodoById(todos, todoId);
+			setTodosToLocalStorage(todos);
+			todo.remove();
+		}
+	});
+};
